b2edca335790cca8c22df49fe7e82bc3cb17a683
Facundo Marin <facundo.marin@moxit.net>
2022-08-04 10:57:47 -0300

    3.1. Inline Event Handlers

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css';
 import GuestList from './state/GuestList';
 import UserSearch from './state/UserSearch';
+import EventComponent from './events/EventComponent';
 
 function App() {
   return (
     <div className="App">
-      <GuestList />
-      <UserSearch />
+      {/* <GuestList />
+      <UserSearch /> */}
+      <EventComponent />
     </div>
   );
 }

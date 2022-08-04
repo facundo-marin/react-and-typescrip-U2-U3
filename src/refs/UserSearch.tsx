510cc97721834a91b69fff112c1a0e5218831a52
Facundo Marin <facundo.marin@moxit.net>
2022-08-04 12:17:10 -0300

    3.5. Applying Types of Refs

---
+++ src/refs/UserSearch.tsx
@@ -0,0 +1,38 @@
+import { useState, useRef } from 'react';
+
+const users = [
+  { name: 'Sarah', age: 20 },
+  { name: 'Alex', age: 20 },
+  { name: 'Michael', age: 20 },
+];
+
+const UserSearch: React.FC = () => {
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const [name, setName] = useState(' ');
+  const [user, setUser] = useState<{ name: string; age: number } | undefined>();
+
+  const onClick = () => {
+    const foundUser = users.find((user) => {
+      return user.name === name;
+    });
+    setUser(foundUser);
+  };
+
+  return (
+    <div>
+      <h3>User Search</h3>
+      <input
+        ref={inputRef}
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button onClick={onClick}>Find User</button>
+      <div>
+        <h5>{user && user.name}</h5>
+        <h5>{user && user.age}</h5>
+      </div>
+    </div>
+  );
+};
+
+export default UserSearch;
